00166494f06ebe05e080442acbb5a3c4749830e1
igdeveloper420 <ap3071674@gmail.com>
2025-11-25 12:55:24 +0530

    string builder

--- src/methods.java
+++ src/methods.java
@@ -4,5 +4,8 @@ public class methods {
         String s ="Ashutosh Pandey";
         System.out.println(Arrays.toString(s.toCharArray()));  // convert string into a
         System.out.println(s.indexOf("A"));
+        System.out.println(s.toLowerCase()); // converts all the letters in small letters
+        System.out.println(s.toUpperCase());  // converts all the letter to capital letters
+        System.out.println(Arrays.toString(s.split(" "))); // automatically splits char whenever it finds a space
     }
 }
