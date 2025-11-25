bf97ac99749cd481dd9ef130164ca374e4de77f7
igdeveloper420 <ap3071674@gmail.com>
2025-11-25 17:49:43 +0530

    Leetcode solution project

--- src/happynumber.java
+++ src/happynumber.java
@@ -1,2 +1,23 @@
+import java.util.*;
+
 public class happynumber {
+    public static void main(String[] args) {
+        int n=2;
+        while(n!=1 && n!=4){
+            int sum=0;
+            while(n>0){
+                int digit=n%10;
+                sum=sum+(digit*digit);
+                n=n/10;
+            }
+            n=sum;
+        }
+        if(n==1){
+            System.out.println("true");
+        }
+        else {
+            System.out.println("false");
+        }
+    }
+
 }
