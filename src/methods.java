1b7f8e0ed9e80185e6c325f342935566520a12c1
igdeveloper420 <ap3071674@gmail.com>
2025-11-25 12:38:29 +0530

    string builder

---
+++ src/methods.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class methods {
+    public static void main(String[] args) {
+        String s ="Ashutosh Pandey";
+        System.out.println(Arrays.toString(s.toCharArray()));  // convert string into a
+        System.out.println(s.indexOf("A"));
+    }
+}
